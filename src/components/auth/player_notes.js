12036d4233b0fcac9f288d67dadad3de9485c90c
darin-estrin <darin.estrin@gmail.com>
2017-06-17 17:03:00 -0700

    players and dm can now leave personal notes about players

--- src/components/auth/player_notes.js
+++ src/components/auth/player_notes.js
@@ -4,7 +4,10 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { TextField, RaisedButton, Paper, List, ListItem } from 'material-ui';
 import { grey900, grey800 } from 'material-ui/styles/colors';
+import ActionNoteAdd from 'material-ui/svg-icons/action/note-add';
 import CampaignNav from './campaign_nav';
+import { getCampaignData, addPlayerNote } from '../../actions';
+require('../../css/player_note.scss');
 
 const styles = {
   underlineStyle: {
@@ -35,16 +38,76 @@ const listStyle = {
 }
 
 class PlayerNotes extends Component {
+  componentWillMount() {
+    const { id, type } = this.props.params;
+    if (!this.props.campaign) {
+      this.props.getCampaignData(id, type);
+    }
+  }
+
+  renderField({
+    input,
+    label,
+    meta: { touched, error },
+    ...custom
+  }) {
+    return (
+      <TextField
+        hintText={label === 'Phone Number' ? '555-555-5555' : label}
+        hintStyle={{color:grey900}}
+        floatingLabelText={label}
+        floatingLabelFocusStyle={{color:'#0097A7'}}
+        underlineStyle={styles.underlineStyle}
+        floatingLabelStyle={styles.floatingLabelStyle}
+        errorText={touched && error}
+        fullWidth
+        inputStyle={{color:grey900}}
+        {...input}
+        {...custom}
+      />
+    );
+  }
+
+  renderPlayerNotes() {
+    var player;
+    const { campaign } = this.props;
+    if (!campaign) { return; } 
+    else { player =_.find(campaign.players, ['characterName', this.props.params.player]); }
+    const notes = player.notes;
+    return notes.map(function(object) {
+      return (
+        <ListItem style={listItemStyle} key={object._id} primaryText={object.note}
+        leftIcon={<ActionNoteAdd/>} />
+      );
+    });
+  }
+
+  handleFormSubmit = ({ note }) => {
+    const { type, id } = this.props.params;
+    const player = _.find(this.props.campaign.players, ['characterName', this.props.params.player]);
+    this.props.addPlayerNote({note, id: player._id, type, campaignId: id});
+    //this.props.reset();
+  }
+
   render() {
+    const { handleSubmit } = this.props;
     return(
       <div>
         <CampaignNav />
         <div className='container'>
           <Paper style={paperStyle}>
-        
+            <List style={listStyle}>
+              <h3 className='notes-header'>Notes for {this.props.params.player}</h3>
+              {this.renderPlayerNotes()}
+            </List>
+            <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+              <div>
+                <Field label='Add a note' name='note' component={this.renderField} />
+              </div>
+              <RaisedButton type='submit' label='Add Note' />
+            </form>
           </Paper>
         </div>
-        
       </div>
     );
   }
@@ -52,10 +115,10 @@ class PlayerNotes extends Component {
 
 function mapStateToProps(state) {
   return {
-
+    campaign: state.user.Campaign
   }
 }
 
 export default reduxForm({
   form: 'player_notes'
-})(connect(mapStateToProps, null)(PlayerNotes));
+})(connect(mapStateToProps, { getCampaignData, addPlayerNote })(PlayerNotes));
