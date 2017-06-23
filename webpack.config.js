170177d23f738864f1ca46e76be12b17656f06ae
darin-estrin <darin.estrin@gmail.com>
2017-06-22 18:29:05 -0700

    minify bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: [
@@ -52,6 +53,12 @@ module.exports = {
   plugins:[
     new HtmlWebpackPlugin({
       template:'src/index.html'
-    })
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 };
