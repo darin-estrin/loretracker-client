f185a23dd2362c6990991afd8d2220bcc950ca5b
darin-estrin <darin.estrin@gmail.com>
2017-08-16 16:41:33 -0700

    added modals for leaving and deleting campaigns

--- src/components/auth/roster.js
+++ src/components/auth/roster.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
-import { TextField, RaisedButton, Paper, List, ListItem }  from 'material-ui';
+import { TextField, RaisedButton, Paper, List, ListItem, Dialog }  from 'material-ui';
 import { grey900 } from 'material-ui/styles/colors';
 import { Link } from 'react-router';
 import { addPlayer, getCampaignData } from '../../actions';
 import CampaignNav from './campaign_nav';
 import * as styles from '../../css/material_styles';
 
-class Roster extends Component {  
+class Roster extends Component {
+  state = {
+    open: false
+  }
+  
   componentWillMount() {
     const { type, id } = this.props.params;
     this.props.getCampaignData(id, type);
@@ -18,6 +22,14 @@ class Roster extends Component {
     const { id } = this.props.params;
     this.props.addPlayer({ email, campaignId: id, name });
     this.props.reset();
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  }
+
+  handleClose = () => {
+    this.setState({ open: false });
   }
 
   renderField({
@@ -43,24 +55,60 @@ class Roster extends Component {
     );
   }
 
+  handleDeleteCampaign = () => {
+    console.log('campaign deleted');
+    this.handleClose();
+  }
+
+  handleLeaveCampaign = () => {
+    console.log('you left the campaign');
+    this.handleClose();
+  }
+
   renderAddPlayer() {
     const { handleSubmit } = this.props;
+    const deleteCampaign = [
+      <RaisedButton
+        label="Cancel"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+      <RaisedButton
+        label="Confirm"
+        secondary={true}
+        onTouchTap={this.handleDeleteCampaign}
+      />,
+    ];
     return (
-      <form onSubmit={handleSubmit(this.addPlayerSubmit)}>
-        <div>
-          <Field label='Player Email' name='email' component={this.renderField} />
-        </div>
-        <div>
-          <Field label='Character Name' name='name' component={this.renderField} />
-        </div>
-        {this.renderAlert()}
-        <RaisedButton label='Add Player' type='submit' />
-        <Link to='/campaigns'>
-          <RaisedButton style={styles.buttonStyle} label='Back to Campaigns' 
-            secondary={true}
-          />
-        </Link>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit(this.addPlayerSubmit)}>
+          <div>
+            <Field label='Player Email' name='email' component={this.renderField} />
+          </div>
+          <div>
+            <Field label='Character Name' name='name' component={this.renderField} />
+          </div>
+          {this.renderAlert()}
+          <RaisedButton label='Add Player' type='submit' />
+          <Link to='/campaigns'>
+            <RaisedButton style={styles.buttonStyle} label='Back to Campaigns' 
+              primary={true}
+            />
+          </Link>
+        </form>
+        <RaisedButton style={{float: 'right', marginTop: '10px'}} secondary={true} 
+        onTouchTap={this.handleOpen} label='Delete Campaign' />
+        <Dialog
+        title="Warning!"
+        actions={deleteCampaign}
+        modal={false}
+        open={this.state.open}
+        onRequestClose={this.handleClose}
+      >
+        This will permanently delete the campaign.
+      </Dialog>
+      </div>
     );
   }
 
@@ -91,6 +139,19 @@ class Roster extends Component {
 
   render() {
     const {id, type} = this.props.params;
+    const leaveCampaign = [
+      <RaisedButton
+        label="Cancel"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+      <RaisedButton
+        label="Confirm"
+        secondary={true}
+        onTouchTap={this.handleLeaveCampaign}
+      />,
+    ];
     return (
       <div>
         <CampaignNav index={0} />
@@ -103,9 +164,21 @@ class Roster extends Component {
                   {this.renderPlayers()}
                 </List>
                 {this.props.params.type ==='dm' ? this.renderAddPlayer() : 
-                <Link to='/campaigns'>
-                  <RaisedButton style={{float:'right', marginTop: '10px'}} label='Back to Campaigns' secondary={true}/>
-                </Link>}
+                <div>
+                  <RaisedButton label='Leave Campaign' style={{marginTop: '10px'}} secondary={true}   onTouchTap={this.handleOpen} />
+                  <Link to='/campaigns'>
+                    <RaisedButton style={{float:'right', marginTop: '10px'}} label='Back to Campaigns' primary={true} />
+                  </Link>
+                  <Dialog
+                  title="Warning!"
+                  actions={leaveCampaign}
+                  modal={false}
+                  open={this.state.open}
+                  onRequestClose={this.handleClose}
+                >
+                  This will permanently remove you from the campaign.
+                </Dialog>
+                </div>}
             </Paper>
           </Paper>
         </div>
